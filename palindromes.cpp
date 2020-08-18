4c4c497ac357977a502e0178f0ca91201f7d7215
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-18 08:45:04 +0530

    REFACTORED Code by providing proper indentation.

--- palindromes.cpp
+++ palindromes.cpp
@@ -8,18 +8,18 @@ int main()
     int num;
     cout << "Enter the Number: \n";
     cin >> num;
-	int r,sum ,temp;
-	temp=num;
-	sum=0;
-	while(temp>0)
+	int reminder, sum, temp;
+	temp = num;
+	sum = 0;
+	while(temp > 0)
 	{
-		r = temp%10;
-		sum = sum*10+r;
-		temp= temp/10;
+		reminder = temp % 10;
+		sum = sum * 10 + reminder;
+		temp = temp / 10;
 	}    
     cout << "Given number: " << num << endl;
     cout << "Reversed number: " << sum << endl;
-	if(num==sum)
+	if(num == sum)
         cout << "Given number is Palindrome." << endl;
     else
         cout << "Given number is not a Palindrome." << endl;
