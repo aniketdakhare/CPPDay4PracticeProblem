12d2bec3b0224308deea2d7ebcbce5fb49386492
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-19 11:11:03 +0530

    REFACTORED Code by providing proper spacing

--- sortingTechnique.cpp
+++ sortingTechnique.cpp
@@ -3,10 +3,10 @@
 #include <ctime>
 using namespace std;
 
-void printArray(int array[], int size)  
+void printArray(int array[], int size)
 {
     cout << "\nSorted Array: ";
-    for (int n = 0; n < size; ++n) 
+    for (int n = 0; n < size; ++n)
     {
         cout << array[n] << "  ";
     }
@@ -25,10 +25,10 @@ void bubbleSort(int array[], int size)
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
                 flag = 1;
-            }   
+            }
         }
         if (flag == 0)
-            break;   
+            break;
     }
 }
 
@@ -38,6 +38,7 @@ void insertionSort(int array[], int size)
     {
         int number = array[i];
         int vacantIndex = i;
+
         while (vacantIndex > 0 && array[vacantIndex - 1] > number)
         {
             array[vacantIndex] = array[vacantIndex - 1];
@@ -62,14 +63,16 @@ void merge(int leftArray[], int leftArraySize, int rightArray[], int rightArrayS
             completeArray[k] = rightArray[j];
             j++;
         }
-        k++;        
+        k++;
     }
+
     while (i < leftArraySize)
     {
         completeArray[k] = leftArray[i];
         i++;
         k++;
     }
+
     while (j < rightArraySize)
     {
         completeArray[k] = rightArray[j];
@@ -84,10 +87,12 @@ void mergeSort(int array[], int size)
     {
         int mid = size / 2;
         int leftArray[mid], rightArray[size - mid];
+
         for (int i = 0; i < mid; i++)
         {
             leftArray[i] = array[i];
         }
+
         for (int i = mid; i < size; i++)
         {
             rightArray[i - mid] = array[i];
@@ -98,11 +103,40 @@ void mergeSort(int array[], int size)
     }
 }
 
+int partition(int array[], int start, int end)
+{
+    int partitionElement = array[end];
+    int partitionIndex = start;
+
+    for (int i = start; i < end; i++)
+    {
+        if (array[i] <= partitionElement)
+        {
+            swap(array[i], array[partitionIndex]);
+            partitionIndex++;
+        }
+    }
+
+    swap(array[partitionIndex], array[end]);
+    return partitionIndex;
+}
+
+void quickSort(int array[], int start, int end)
+{
+    if (start < end)
+    {
+        int partitionIndex = partition (array, start, end);
+        quickSort(array, start, (partitionIndex - 1));
+        quickSort(array, (partitionIndex + 1), end);
+    }
+}
+
 void selectSortType(int array[], int size)
 {
     int select;
     cout << "\nSelect your choice of sorting: \n1: Bubble Sort \n2: Insertion sort \n3: Quick sort \n4: Merge sort\n";
     cin >> select;
+
     switch(select)
     {
         case 1:
@@ -111,12 +145,16 @@ void selectSortType(int array[], int size)
         case 2:
             insertionSort(array, size);
             break;
+        case 3:
+            quickSort(array, 0, (size - 1));
+            break;
         case 4:
             mergeSort(array, size);
             break;
         default:
             cout << "\nInvalid Input" << endl;
     }
+
     printArray(array, size);
 }
 
@@ -125,11 +163,14 @@ int main()
     int size;
     cout << "\nEnter the size of an Array: \n";
     cin >> size;
+
     int userArray[size];
     cout << "\nEnter elements of array: \n";
+
     for (int i = 0; i < size; i++)
     {
         cin >> userArray[i];
     }
+
     selectSortType(userArray, size);
 }
